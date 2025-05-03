bb3eb8d39c50bd44b9a427fd38480d4a43c91e24
skipdudes <skipdudes.dev@gmail.com>
2025-05-03 02:14:56 +0200

    Provided some comments

--- ChatServer/server.py
+++ ChatServer/server.py
@@ -6,22 +6,25 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 from groq import Groq
 
+# FastAPI entrypoint
 app = FastAPI()
 
+# Load .env file data (Groq Api Key)
 load_dotenv()
 
-client = Groq(
-    api_key=os.getenv('GROQ_API_KEY'),
-)
+# Groq client instance (created with loaded Api Key)
+client = Groq(api_key = os.getenv('GROQ_API_KEY'))
 
-
+# Message class
 class Message(BaseModel):
     role: str
     content: str
 
+# Request Data class
 class RequestData(BaseModel):
     messages: list[Message]
 
+# Inner function invoking chosen language model
 def generate_response(messages):
     chat_completion = client.chat.completions.create(
         messages=[message.dict() for message in messages],
@@ -30,12 +33,15 @@ def generate_response(messages):
 
     return chat_completion.choices[0].message.content
 
+# Proper function to send model response to the game
 @app.post("/generate/")
 async def generate_response_endpoint(data: RequestData):
     response = generate_response(data.messages)
     print("User prompt received: ", data.messages[-1].content)
+    print("Answering with: ", response)
+
     return response
 
+# Start local server at program start up
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
